4dd0447f43d2a831049c3009f0e4dbe15bae78dc
oleksasa <oleksandrasevcuk04@gmail.com>
2024-03-25 17:21:37 +0200

    hw

---
+++ src/ContactList/ContactList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Contact from "../Contact/Contact";
+import s from "./ContactList.module.css";
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul className={s.ContactList}>
+      {contacts?.map((contact) => (
+        <Contact
+          key={contact.id}
+          contact={contact}
+          onDeleteContact={onDeleteContact}
+        />
+      ))}
+    </ul>
+  );
+};
+export default ContactList;
---
+++ src/SearchBox.jsx/SearchBox.jsx
@@ -0,0 +1,20 @@
+import { Form, Formik } from "formik";
+import React from "react";
+import s from './SearchBox.module.css'
+
+const SearchBox = ({ onChangeFilter, filter }) => {
+  return (
+    <label className={s.SearchBox}>
+      <span className={s.SearchTitle}>Find contacts by name</span>
+      <input
+        className={s.SearchInput}
+        type="name"
+        name="findUser"
+        onChange={onChangeFilter}
+        value={filter}
+      />
+    </label>
+  );
+};
+
+export default SearchBox;
